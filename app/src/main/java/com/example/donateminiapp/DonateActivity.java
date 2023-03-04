d7345918da92219d3eeac64acedc0f02c7ac0169
Stafford009 <stafford009@gannon.edu>
2023-03-03 23:41:07 -0500

    Added a subpar options menu

--- app/src/main/java/com/example/donateminiapp/DonateActivity.java
+++ app/src/main/java/com/example/donateminiapp/DonateActivity.java
@@ -1,9 +1,14 @@
 package com.example.donateminiapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,6 +40,31 @@ public class DonateActivity extends AppCompatActivity {
     private boolean GoalAchieved = false;
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.donate:
+                Intent intent1 = new Intent(getApplicationContext(), DonateActivity.class);
+                startActivity(intent1);
+                Toast.makeText(this, "Donation Screen Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.report:
+                Intent intent2 = new Intent(getApplicationContext(), ReportActivity.class);
+                startActivity(intent2);
+                Toast.makeText(this, "Report Screen Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,6 +125,8 @@ public class DonateActivity extends AppCompatActivity {
                 }
             }
         });
+
+
 
 
     }
--- app/src/main/java/com/example/donateminiapp/ReportActivity.java
+++ app/src/main/java/com/example/donateminiapp/ReportActivity.java
@@ -3,6 +3,8 @@ package com.example.donateminiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
