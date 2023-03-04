326588d7dc7bc063c046a0eb176b0926bd614eba
Stafford009 <stafford009@gannon.edu>
2023-03-03 23:42:30 -0500

    added options menu to report menu

--- app/src/main/java/com/example/donateminiapp/ReportActivity.java
+++ app/src/main/java/com/example/donateminiapp/ReportActivity.java
@@ -1,10 +1,14 @@
 package com.example.donateminiapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -19,5 +23,24 @@ public class ReportActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.donate:
+                Intent intent1 = new Intent(getApplicationContext(), DonateActivity.class);
+                startActivity(intent1);
+                Toast.makeText(this, "Donation Screen Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.report:
+                Intent intent2 = new Intent(getApplicationContext(), ReportActivity.class);
+                startActivity(intent2);
+                Toast.makeText(this, "Report Screen Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
